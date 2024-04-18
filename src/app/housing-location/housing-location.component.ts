187b644f35fbea5ce3f2a0b3641954e095817be4
JoelFaldin <joelfaldin@gmail.com>
2024-04-17 23:21:20 -0400

    refactor: configuring an achor element to allow routing on the app

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { HousingLocation } from '../housinglocation';
+import { RouterModule, RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule, RouterLink, RouterOutlet],
   template: `
     <section class="listing">
       <img
@@ -16,6 +17,7 @@ import { HousingLocation } from '../housinglocation';
       />
       <h2 class="listing-heading">{{ housingLocation.name }}</h2>
       <p class="listing-location">{{ housingLocation.city }}, {{ housingLocation.state }}</p>
+      <a [routerLink]="['/details', housingLocation.id]">Learn More</a>
     </section>
   `,
   styleUrls: ['./housing-location.component.css']

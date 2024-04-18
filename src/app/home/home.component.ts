590d857320afd89df8393b567b28a0c169cf809d
JoelFaldin <joelfaldin@gmail.com>
2024-04-18 18:25:23 -0400

    feat: adding a debounce for the filter Now the filter will make a request to the database 300ms after a keystroke is inputted. (^ｰ^)

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,17 +1,20 @@
-import {Component, inject} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {HousingLocationComponent} from '../housing-location/housing-location.component';
-import {HousingLocation} from '../housinglocation';
-import {HousingService} from '../housing.service';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { HousingLocation } from '../housinglocation';
+import { HousingService } from '../housing.service';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { debounceTime, distinctUntilChanged, filter } from 'rxjs';
+
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, HousingLocationComponent],
+  imports: [CommonModule, HousingLocationComponent, ReactiveFormsModule],
   template: `
     <section>
       <form>
-        <input type="text" placeholder="Filter by city" #filter />
-        <button class="primary" type="button" (click)="filterResults(filter.value)">Search</button>
+        <input type="text" placeholder="Filter by city" #filter [formControl]="filterTimer" />
+        <button class="primary" type="button">Search</button>
       </form>
     </section>
     <section class="results">
@@ -23,21 +26,39 @@ import {HousingService} from '../housing.service';
   `,
   styleUrls: ['./home.component.css'],
 })
+
 export class HomeComponent {
+  filterTimer = new FormControl('');
+
   housingLocationList: HousingLocation[] = [];
   housingService: HousingService = inject(HousingService);
   filteredLocationList: HousingLocation[] = [];
+
   constructor() {
     this.housingService.getAllHousingLocations().then((housingLocationList: HousingLocation[]) => {
       this.housingLocationList = housingLocationList;
       this.filteredLocationList = housingLocationList;
     });
   }
-  filterResults(text: string) {
+
+  ngOnInit(): void {
+    this.filterTimer.valueChanges
+      .pipe(
+        debounceTime(300),
+        distinctUntilChanged(),
+        filter(text => text !== null && (text.length === 0 || text.length > 2))
+      )
+      .subscribe(value => {
+        this.filterResults(value);
+      });
+  }
+
+  filterResults(text: string | null) {
     if (!text) {
       this.filteredLocationList = this.housingLocationList;
       return;
     }
+
     this.filteredLocationList = this.housingLocationList.filter(
       (housingLocation) => housingLocation?.city.toLowerCase().includes(text.toLowerCase()),
     );

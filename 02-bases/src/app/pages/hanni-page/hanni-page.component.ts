e095b367709650b3b9768272774a511f956a6562
JoelFaldin <joelfaldin@gmail.com>
2025-04-02 16:47:16 -0300

    feat: rendering list item if coolness is over 50

--- 02-bases/src/app/pages/hanni-page/hanni-page.component.ts
+++ 02-bases/src/app/pages/hanni-page/hanni-page.component.ts
@@ -16,7 +16,7 @@ interface Outfits {
 export class HanniPageComponent {
   outfits = signal<Outfits[]>([
     { id: 1, song: 'Go hanni', coolness: 100 },
-    { id: 2, song: 'New Jeans', coolness: 60 },
+    { id: 2, song: 'New Jeans', coolness: 50 },
     { id: 3, song: 'Right Now', coolness: 80 },
     { id: 4, song: 'OMG', coolness: 60 },
   ]);

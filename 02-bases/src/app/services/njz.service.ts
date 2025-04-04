ea9739a6feeb5764f4aa7b44a8275960c0b1a34d
JoelFaldin <joelfaldin@gmail.com>
2025-04-04 11:13:34 -0300

    feat: saving data to localstorage and reading from it

--- 02-bases/src/app/services/njz.service.ts
+++ 02-bases/src/app/services/njz.service.ts
@@ -1,12 +1,20 @@
-import { Injectable, signal } from "@angular/core";
+import { effect, Injectable, signal } from "@angular/core";
 import { Outfit } from "../interfaces/outfit.interface";
+
+const loadFromLocalStorage = () => {
+  const outfits = localStorage.getItem('outfits');
+
+  return outfits ? JSON.parse(outfits) : [];
+}
 
 @Injectable({ providedIn: 'root' })
 export class NJZService {
-  outfits = signal<Outfit[]>([
-    { id: 1, song: 'Go hanni', coolness: 100 },
-    { id: 2, song: 'New Jeans', coolness: 70 },
-  ]);
+  outfits = signal<Outfit[]>(loadFromLocalStorage());
+
+  saveToLocalStorage = effect(() => {
+    console.log('outfit count: ' + this.outfits().length);
+    localStorage.setItem('outfits', JSON.stringify(this.outfits()));
+  })
 
   addOutfit(outfit: Outfit) {
     this.outfits.update(prev => [...prev, outfit]);
